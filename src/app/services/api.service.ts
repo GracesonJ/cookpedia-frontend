c5ec8fb8cb264fbe03d6fe28562b3ff8692699ec
Graceson J <gracesonjjohnson@gmail.com>
2025-02-13 15:34:18 +0530

    add testimony

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -13,4 +13,9 @@ export class ApiService {
   getAllRecipesApi(){
    return this.http.get(`${this.server_url}/all-recipes`)
   }
+
+  // add-testimony
+  addTestimonyApi(reqBody:any){
+    return this.http.post(`${this.server_url}/add-testimony`, reqBody)
+  }
 }
