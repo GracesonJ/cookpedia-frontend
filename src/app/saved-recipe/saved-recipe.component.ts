326472ca4c09cf20f4c452a55add17cb8dd55b7e
Graceson J <gracesonjjohnson@gmail.com>
2025-02-21 15:03:04 +0530

    profile

--- src/app/saved-recipe/saved-recipe.component.ts
+++ src/app/saved-recipe/saved-recipe.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-saved-recipe',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './saved-recipe.component.html',
   styleUrl: './saved-recipe.component.css'
 })
 export class SavedRecipeComponent {
+  // create a property to store array of saved recipes
+  allRecipes:any = []
+  // api service dependency injuction
+  constructor(private api:ApiService){}
+  // call the function inside ngOnInit
+  ngOnInit(){
+    this.getAllSavedRecipes()
+  }
 
+  // define a function for call saveRecipe api using api service
+  getAllSavedRecipes(){
+    this.api.getUserSaveRecipesAPI().subscribe((res:any)=>{
+      this.allRecipes = res
+      console.log(this.allRecipes);
+      
+    })
+  }
+  removeSaveRecipe(id:string){
+    this.api.deleteSaveRecipeAPI(id).subscribe((res:any)=>{
+      this.getAllSavedRecipes()
+    })
+  }
+ 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,57 +8,76 @@ export class ApiService {
 
   server_url = "http://localhost:3000"
 
-  constructor(private http:HttpClient) { }
-  
-  getAllRecipesApi(){
-   return this.http.get(`${this.server_url}/all-recipes`)
+  constructor(private http: HttpClient) { }
+
+  getAllRecipesApi() {
+    return this.http.get(`${this.server_url}/all-recipes`)
   }
 
   // add-testimony
-  addTestimonyApi(reqBody:any){
+  addTestimonyApi(reqBody: any) {
     return this.http.post(`${this.server_url}/add-testimony`, reqBody)
   }
 
   // register
-  registerApi(reqBody:any){
+  registerApi(reqBody: any) {
     return this.http.post(`${this.server_url}/register`, reqBody)
   }
 
   // login
-  loginApi(reqBody:any){
+  loginApi(reqBody: any) {
     return this.http.post(`${this.server_url}/login`, reqBody)
   }
 
   // appendToken in req header
-  appendToken(){
+  appendToken() {
     let headers = new HttpHeaders()
     const token = sessionStorage.getItem("token")
-    if(token){
+    if (token) {
       headers = headers.append("Authorization", `Bearer ${token}`)
     }
-    return {headers}
+    return { headers }
   }
 
   // View Recipe api
-  viewRecipeApi(recipeId:string){
+  viewRecipeApi(recipeId: string) {
     return this.http.get(`${this.server_url}/recipe/${recipeId}/view`, this.appendToken())
   }
 
   // related Recipe Api
-  relatedRecipeApi(cuisine:string){
+  relatedRecipeApi(cuisine: string) {
     return this.http.get(`${this.server_url}/related-recipes?cuisine=${cuisine}`, this.appendToken())
   }
 
   // Download Recipe Api
   // recipe/67b44c0cdff57192daf7c410/download
-  downloadRecipeApi(recipeId:any, reqBody:any){
+  downloadRecipeApi(recipeId: any, reqBody: any) {
     return this.http.post(`${this.server_url}/recipe/${recipeId}/download`, reqBody, this.appendToken())
   }
-  
- //recipe/:id/save
- saveRecipeAPI(recipeId:string,reqBody:any){
-  return this.http.post(`${this.server_url}/recipe/${recipeId}/save`,reqBody,this.appendToken())
-}
 
+  //recipe/:id/save
+  saveRecipeAPI(recipeId: string, reqBody: any) {
+    return this.http.post(`${this.server_url}/recipe/${recipeId}/save`, reqBody, this.appendToken())
+  }
+
+  //get-save-recipes
+  getUserSaveRecipesAPI() {
+    return this.http.get(`${this.server_url}/get-save-recipes`, this.appendToken())
+  }
+
+  //save-recipes/6756c4a46917775a90386a45/remove
+  deleteSaveRecipeAPI(id: string) {
+    return this.http.delete(`${this.server_url}/save-recipes/${id}/remove`, this.appendToken())
+  }
+
+  //user-downloads
+  getUserDownloadRecipesAPI() {
+    return this.http.get(`${this.server_url}/user-downloads`, this.appendToken())
+  }
+
+  //user/edit
+  editUserAPI(reqBody:any){
+    return this.http.post(`${this.server_url}/user/edit`, reqBody, this.appendToken())
+  }
 
 }
